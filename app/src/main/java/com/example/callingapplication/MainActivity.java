187dfe1d2095daf49d945419fe8fc5321efd0bc4
mys_ya_ <7nacu7@gmail.com>
2021-06-12 01:13:55 +0300

    Простое приложение для отправки данных из одного app в другое. Просто ради функционала.

--- app/src/main/java/com/example/callingapplication/MainActivity.java
+++ app/src/main/java/com/example/callingapplication/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.callingapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String VALUE_SEND = "VALUE_SEND";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        final EditText sendValueEditText = findViewById(R.id.valuesCalling);
+        Button sendValueButton = findViewById(R.id.buttonSend);
+        sendValueButton.setOnClickListener(v -> {
+            Uri uri = Uri.parse("example://intent");
+            Intent runEchoIntent = new Intent(Intent.ACTION_VIEW, uri);
+            runEchoIntent.putExtra(VALUE_SEND, sendValueEditText.getText().toString());
+            ActivityInfo activityInfo =
+                    runEchoIntent.resolveActivityInfo(getPackageManager(),
+                            runEchoIntent.getFlags());
+            if (activityInfo != null) {
+                startActivity(runEchoIntent);
+            }
+        });
     }
 }
